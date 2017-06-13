2112cea89b85dda2e3dab844e99263ce229dfa74
Shen Han <shehan01@shawn-server.shanghai.arm.com>
2017-06-13 18:56:41 +0800

    fm

--- vars/arg_converter.groovy
+++ vars/arg_converter.groovy
@@ -4,7 +4,7 @@ def call(LinkedHashMap para){
     arg = []
     def para_key_set = para.keySet()
     for (int i = 0; i < para.size(); i++){
-        if ( para[para_key_set[i]].getClass() == String.TYPE ){
+        if ( para[para_key_set[i]] instanceof String){
             def tmp = [$class: 'StringParameterValue', name: para_key_set[i], value: para[para_key_set[i]]]
             arg.add(tmp)
         }
