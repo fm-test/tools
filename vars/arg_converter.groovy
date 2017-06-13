95683b42d271598c1056e28826e8b559786d7f02
Shen Han <shehan01@shawn-server.shanghai.arm.com>
2017-06-13 14:43:13 +0800

    fm

--- vars/arg_converter.groovy
+++ vars/arg_converter.groovy
@@ -1,7 +1,8 @@
 #!/usr/bin/env groovy
 
-def call(HashMap para){
+def call(List para){
     arg = []
+    para= para[0]
     def para_key_set = para.KeySet()
     for (int i = 0; i < para.size(); i ++){d
         def tmp = [$class: 'StringParameterValue', name: para_key_set[i], value: para[para_key_set[i]]]
